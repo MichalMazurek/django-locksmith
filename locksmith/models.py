c4ac4c5b993423fee67fdb0ce8938a49ea667c66
michal <mazurek.michal@gmail.com>
2014-06-15 12:02:09 +0200

    adding cache to key checks

--- locksmith/models.py
+++ locksmith/models.py
@@ -15,14 +15,23 @@ class KeyChain(models.Model):
     keys = models.ManyToManyField(Key, blank=True)
     expiration_date = models.DateField(verbose_name="Expiration date")
 
+    _cache = {}
+
     def is_expired(self):
         return datetime.datetime.now().date() >= self.expiration_date
 
     def __contains__(self, key_name):
 
-        try:
-            return self.keys.get(name=key_name) is not None
-        except ObjectDoesNotExist:
+        if len(self._cache) == 0:
+            def set_cache(name, value):
+                nonlocal self
+                self._cache[name] = value
+
+            [set_cache(key.name, key) for key in self.keys.all()]
+
+        if key_name in self._cache:
+            return True
+        else:
             return False
 
 
