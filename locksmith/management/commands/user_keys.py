149f53c141b9ee17d3849dab48bab8c2aa56b4d4
michal <mazurek.michal@gmail.com>
2014-07-01 14:25:04 +0200

    adding command to add or remove keys for user, removed old add key command

--- locksmith/management/commands/user_keys.py
+++ locksmith/management/commands/user_keys.py
@@ -8,10 +8,12 @@ from optparse import make_option
 
 class Command(BaseCommand):
     can_import_settings = True
-    help = "Adds keys to users"
+    help = "Adds or removes keys to users"
     args = "<key key key ...>"
     option_list = BaseCommand.option_list + (
         make_option("--user-pk", dest="user_pk"),
+        make_option("--remove", dest="remove", default=False,
+                    action="store_true"),
         make_option("--expiration-date", dest="expiration_date",
                     help="Keychain expiration date, format: YYYY-MM-DD")
 
@@ -25,7 +27,7 @@ class Command(BaseCommand):
                             help="Primary key of user")
 
         parser.add_argument('--expiration-date', dest="expiration_date",
-                            help="Keychain expiration date, format: 2014-12-31"
+                            help="Keychain expiration date, format: YYYY-MM-DD"
         )
 
     def handle(self, *args, **kwargs):
@@ -55,10 +57,17 @@ class Command(BaseCommand):
         for key_name in args:
             try:
                 key = Key.objects.get(name=key_name)
-                keychain.keys.add(key)
-                self.stdout.write("Adding key '%s' to '%s'" %
-                                  (key_name, user.get_full_name()))
+                if kwargs['remove']:
+                    keychain.keys.remove(key)
+                    self.stdout.write("Removing key '%s' from '%s'" %
+                                      (key_name, user.get_full_name()))
+                else:
+                    keychain.keys.add(key)
+
+                    self.stdout.write("Adding key '%s' to '%s'" %
+                                      (key_name, user.get_full_name()))
+
             except Key.DoesNotExist:
-                self.stderr("Key '%s' does not exist" % key_name)
+                self.stderr.write("Key '%s' does not exist" % key_name)
 
         keychain.save()
